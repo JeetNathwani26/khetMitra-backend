ebcbc9fa24b0de7339f39f2e904d98eb136fb793
jeetnathwani26 <jeetnathwani660@gmail.com>
2025-12-10 10:05:31 +0530

    change the file

--- api/file/database.py
+++ api/file/database.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient  # type: ignore
 
 def get_db(db_name):
-    client=MongoClient("mongodb+srv://jeetnathwani660_db_user:YfvCmokx5AZMOX5T@cluster0.mc28jjy.mongodb.net/?appName=Cluster0")
-    db=client[db_name]
+    client = MongoClient(
+        "mongodb+srv://jeetnathwani660_db_user:YfvCmokx5AZMOX5T@cluster0.mc28jjy.mongodb.net/"
+        "?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true"
+    )
+    db = client[db_name]
     return db
