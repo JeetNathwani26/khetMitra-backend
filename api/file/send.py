9960ee53f2baa47627eb73da13999e81058c39e9
jeetnathwani26 <jeetnathwani660@gmail.com>
2025-12-10 18:22:59 +0530

    change the file

--- api/file/send.py
+++ api/file/send.py
@@ -5,12 +5,13 @@ resend.api_key = "re_9PkoGJau_PSgVmtuWtU2cM6pVK2oSv8ct"
 def send(data):
     try:
         email = resend.Emails.send({
-            "from": "KhetMitra26@gmail.com",
-            "to": data["to"],
+            "from": "KhetMitra <onboarding@resend.dev>",
+            "to": [data["to"]],                     # MUST be list
             "subject": "KhetMitra",
             "html": data["text"]
         })
-        return {"status": "mail sent"}
+        return {"status": "mail sent", "id": email["id"]}
+
     except Exception as e:
-        print(e)
+        print("Email Error:", e)
         return {"status": "mail not sent"}
